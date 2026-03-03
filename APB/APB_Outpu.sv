f14db1e1b409649237df065bcb06b6033b9e8c48
Nitesh-KK <niteshkk.github@gmail.com>
2026-03-03 19:12:57 +0530

    Create APB_Outpu.sv

---
+++ APB/APB_Outpu.sv
@@ -0,0 +1,23 @@
+VCD info: dumpfile APB_waves.vcd opened for output.
+VCD warning: $dumpvars: Package ($unit) is not dumpable with VCD.
+-----Write-----
+Address=fa Data=eeee
+-----Write-----
+Address=ba Data=9999
+-----Write-----
+Address=ac Data=cccc
+-----Write-----
+Address=ce Data=5555
+-----Read-----
+Address=fa Data=eeee
+-----Read-----
+Address=ba Data=9999
+-----Read-----
+Address=ac Data=cccc
+-----Read-----
+Address=ce Data=5555
+testbench.sv:45: $finish called at 218 (1s)
+Finding VCD file...
+./APB_waves.vcd
+[2026-02-24 07:09:31 UTC] Opening EPWave...
+Done
